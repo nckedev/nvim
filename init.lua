e3a70d8eea2ac0c3b34e11c8b407fa456cc8d9db
nckedev <nmk411@gmail.com>
2024-10-08 21:22:12 +0200

    autopairs insert () on function completion

--- init.lua
+++ init.lua
@@ -978,7 +978,16 @@ require("lazy").setup({
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		config = true,
+		--		config = true,
+		-- Optional dependency
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- If you want to automatically add `(` after selecting a function or method
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
 		-- use opts = {} for passing setup options
 		-- this is equivalent to setup({}) function
 	},
