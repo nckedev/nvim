b68bc3a35ab01596b85fe6b3a23c01b695205fb4
nckedev <nmk411@gmail.com>
2026-01-06 17:10:07 +0100

    Wichkey fix

--- init.lua
+++ init.lua
@@ -300,13 +300,11 @@ require("lazy").setup({
         require("which-key").setup({ delay = 500 })
 
         -- Document existing key chains
-        require("which-key").register({
-          ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-          ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-          ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-          ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-          ["<leader>f"] = { name = "Find", _ = "which_key_ignore" },
-          ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+        require("which-key").add({
+          { "<leader>c", group = "[C]ode" },
+          { "<leader>d", group = "[D]ocument" },
+          { "<leader>r", group = "[R]ename" },
+          { "<leader>f", group = "[F]ind" },
         })
       end,
     },
