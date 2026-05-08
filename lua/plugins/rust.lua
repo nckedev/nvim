8910ce0e1eed487fa36395d885bf564e57c14104
nckedev <nmk411@gmail.com>
2026-05-08 21:55:57 +0200

    rust fix

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,7 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^6', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  version = '^9', -- Recommended
+  lazy = false,   -- This plugin is already lazy
 }
+
+-- TODO: rust-analyzer.references.excludeImports
